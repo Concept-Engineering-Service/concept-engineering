6a538385e2c54f8393f5b438bc05a28463667b85
Anzar <peerzada.anzar@gmail.com>
2025-04-13 19:42:35 +0530

    enable hash routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter} from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes as appRoutes } from './app.routes';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(appRoutes)
+    provideRouter(appRoutes, withHashLocation())
   ]
 };
 
